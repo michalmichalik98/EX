24ef49ab21374a25226f6456abb39b36c7209039
Michał Michalik <michalek.m21@gmail.com>
2022-11-15 20:06:48 +0100

    chnges in class Score

--- Ex2/src/com/isa/bootcamp/BootcampApp.java
+++ Ex2/src/com/isa/bootcamp/BootcampApp.java
@@ -11,25 +11,23 @@ public class BootcampApp {
         ex2.setExnum("Ex2");
         ex2.setDescription("Podaj wynik mnożenia liczb 6 i 3.");
         ex2.setPoints(10);
-
+        
 
         Student ania = new Student();
         Student jan = new Student();
+
         ania.setName("Ania");
         ania.setSecondName("Nowak");
         ania.setGitHub("anianowak");
-
         jan.setName("Jan");
         jan.setSecondName("Kowalski");
         jan.setGitHub("jankowalski");
 
-
-
         Score max = new Score();
         Score min = new Score();
 
-        min.getFeedBack("Zadanie oddane po terminie");
-        max.getFeedBack("Zadnia wykonane bardzo dobrze");
+        min.setFeedBack("Zadanie oddane po terminie");
+        max.setFeedBack("Zadnia wykonane bardzo dobrze");
         max.setPoints(10);
         min.setPoints(5);
 
--- Ex2/src/com/isa/bootcamp/Score.java
+++ Ex2/src/com/isa/bootcamp/Score.java
@@ -3,6 +3,10 @@ package com.isa.bootcamp;
 public class Score {
     private Integer points;
     private  String  feedBack;
+    private Student student;
+    private Task task;
+
+
 
     public Integer getPoints() {
         return points;
@@ -18,5 +22,21 @@ public class Score {
 
     public void setFeedBack(String feedBack) {
         this.feedBack = feedBack;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
     }
 }
