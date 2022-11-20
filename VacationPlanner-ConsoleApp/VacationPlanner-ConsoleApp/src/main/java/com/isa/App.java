a81e81ef0f9645e686544b5c41621bf169ccbc7b
Dominik Szlachetka <szlachetka.dominik@gmail.com>
2022-11-20 19:16:04 +0100

    szkielet aplikacji JJDZR8LB-2

---
+++ VacationPlanner-ConsoleApp/VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
@@ -0,0 +1,13 @@
+package com.isa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "*Luz_Blue Team*" + "\n" + "Hello!" + "\n" + "Welcome to the VacationPlanner" );
+    }
+}
