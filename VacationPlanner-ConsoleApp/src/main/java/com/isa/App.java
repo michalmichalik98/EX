764a817b886d98455d313cf044f3c5ccaffde897
Adrian Debowski <adrian.debowski@outlook.com>
2022-11-27 13:27:04 +0100

    First iteration of menu and behaviour added.

--- VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
@@ -1,9 +1,19 @@
 package com.isa;
 
-public class App 
+import com.isa.menu.MenuBehaviour;
+import com.isa.menu.MenuList;
+
+public class App
 {
     public static void main( String[] args )
     {
+
+
         System.out.println( "*Luz_Blue Team*" + "\n" + "Hello!" + "\n" + "Welcome to the VacationPlanner" );
+
+        MenuBehaviour menu = new MenuBehaviour();
+        menu.retrieveSelection();
+
+
     }
 }
---
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/menu/MenuBehaviour.java
@@ -0,0 +1,58 @@
+package com.isa.menu;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MenuBehaviour {
+
+    MenuList list = new MenuList();
+
+    public void retrieveSelection() {
+
+
+        int value = 0;
+        boolean stayInLoop = true;
+        do {
+            list.printOption();
+            try {
+                Scanner scanner = new Scanner(System.in);
+                value = scanner.nextInt();
+                behaviour(value);
+                if (value == 0) {
+                    stayInLoop = false;
+                }
+            } catch (InputMismatchException ex){
+                System.out.println("Podaj cyfrę.\n");
+            } catch (Exception e) {
+                System.out.println("Niespotykany błąd.\n");
+            }
+        } while (stayInLoop);
+    }
+
+       public void behaviour(int value){
+            switch (value) {
+                case 1:
+                    System.out.println("Dodaje Urlop.\n");
+                    break;
+                case 2:
+                    System.out.println("Dodaje Pracownika.\n");
+                    break;
+                case 3:
+                    System.out.println("Usuwam Pracownika.\n");
+                    break;
+                case 4:
+                    System.out.println("Dodaje Zespół.\n");
+                    break;
+                case 5:
+                    System.out.println("Usuwam Zespół.\n");
+                    break;
+                case 0:
+                    System.out.println("Wychodzę!\n");
+                    break;
+                default:
+                    System.out.println("Nie ma takiej opcji.\n");
+                    break;
+
+            }
+    }
+}
