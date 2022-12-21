f360fa5c6a45e021dbfdb126c0d070a8d192d38d
PatrykGwardzik <gwardzikpatryk@gmail.com>
2022-12-21 23:01:32 +0100

    Jason dependencis added to maven Pom file

--- VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
@@ -1,6 +1,9 @@
 package com.isa;
 
 import com.isa.menu.MenuBehaviour;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App
 {
---
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/dataManager/DataManager.java
@@ -0,0 +1,4 @@
+package com.isa.dataManager;
+
+public class DataManager {
+}
