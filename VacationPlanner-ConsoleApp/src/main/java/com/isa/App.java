be435845d0912435ee770f94aa88030392cef0e7
PatrykGwardzik <gwardzikpatryk@gmail.com>
2023-01-13 15:41:21 +0100

    comite befour checkout

--- VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/App.java
@@ -9,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "*Luz_Blue Team*" + "\n" + "Hello!" + "\n" + "Welcome to the VacationPlanner\n" );
 
         MenuBehaviour menu = new MenuBehaviour();
