a52bf2d487b810f542b16f1d541e534e0f7d2144
PatrykGwardzik <gwardzikpatryk@gmail.com>
2023-01-19 20:02:32 +0100

    AddEmploee halfworking

--- VacationPlanner-ConsoleApp/src/main/java/com/isa/menu/AddEmployeeToTeam.java
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/menu/AddEmployeeToTeam.java
@@ -4,9 +4,12 @@ import com.isa.dataManager.DataManager;
 import com.isa.model.Employee;
 import com.isa.model.Team;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AddEmployeeToTeam {
+    Map<Integer, Integer> unassignedPositionsMap = new HashMap<>();
 
     public void add() {
         int exit = 1;
@@ -15,12 +18,18 @@ public class AddEmployeeToTeam {
             System.out.println("Pracownicy bez zespołu:");
             printUnassignedEmployees();
             System.out.print("Podaj numer pracownika: ");
-            int employeeSelected = selectEmployee();
+            setUnassignedPositionsMap();
+            System.out.println(unassignedPositionsMap);
 
+            int selectin = selectEmployee();
+            System.out.println("To wybrałes" + selectin);
+
+            int employeeSelected = unassignedPositionsMap.get(selectin);
+            System.out.println("taki value dostałes od klucza" + employeeSelected);
             System.out.println("\nDostępne zespoły");
             printAllTeams();
             System.out.print("Wybież numer zestołu do którego przypisać pracownika: ");
-            int teamSelected = userInput() ;
+            int teamSelected = userInput();
 
             DataManager.employeeList.get(employeeSelected)
                     .setTeam(DataManager.teamList.get(teamSelected).getTeamName());
@@ -29,12 +38,12 @@ public class AddEmployeeToTeam {
                     DataManager.employeeList.get(employeeSelected).getName() + " " +
                     DataManager.employeeList.get(employeeSelected).getSurname() + " został dodany do zespołu " +
                     DataManager.employeeList.get(employeeSelected).getTeam());
-            exit = employeeSelected;
+            //exit = employeeSelected;
         }
     }
 
     private int selectEmployee() {
-        int employeeSelected = userInput()-1 ;
+        int employeeSelected = userInput()-1;
         System.out.println("Wybrałeś: "
                 + DataManager.employeeList.get(employeeSelected).getName() + " "
                 + DataManager.employeeList.get(employeeSelected).getSurname());
@@ -42,16 +51,28 @@ public class AddEmployeeToTeam {
     }
 
     private void printUnassignedEmployees() {
-
         int i = 1;
-
         for (Employee employee : DataManager.employeeList) {
             if (employee.getTeam().equals("UnAssignedTeam")) {
                 System.out.println(i++ + ". " + employee.getName() + " " + employee.getSurname());
             }
         }
         System.out.println("0. Wyjście");
+    }
 
+    private void setUnassignedPositionsMap() {
+        int order = 1;
+        int unassignedPosition = 0;
+        Map<Integer, Integer> unassignedPositionsMap = new HashMap<>();
+
+        for (Employee employee : DataManager.employeeList) {
+
+            if (employee.getTeam().equals("UnAssignedTeam")) {
+                unassignedPositionsMap.put(order++,unassignedPosition);
+            }
+            unassignedPosition++;
+        }
+        this.unassignedPositionsMap = unassignedPositionsMap;
     }
 
     public int userInput() {
