0b15effc2535f4489b72a4ac4b73bf1328accdfe
PatrykGwardzik <gwardzikpatryk@gmail.com>
2023-01-19 14:52:13 +0100

    Deleteing Employee has been Added

--- VacationPlanner-ConsoleApp/src/main/java/com/isa/menu/DeleteEmployee.java
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/menu/DeleteEmployee.java
@@ -1,9 +1,31 @@
 package com.isa.menu;
 
 import com.isa.dataManager.DataManager;
+import com.isa.model.Employee;
+
+import java.util.Scanner;
 
 public class DeleteEmployee {
-    public void delete(){
+    public void delete() {
+        System.out.println("Podaj PESEL pracownika do usunięcia: ");
 
+        String pesel = userInput();
+        for (Employee employee : DataManager.employeeList) {
+
+            if(employee.getPesel().equals(pesel)) {
+                DataManager.employeeList.remove(employee);
+                System.out.println("Pracownik" + employee.getName() + employee.getSurname() + "został usunięty/a");
+                break;
+            }
+        }
+        System.out.println("Nie ma pracownika o takim numerze PESEL");
     }
+
+    public String userInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        input = scanner.nextLine();
+        return input;
+    }
+
 }
