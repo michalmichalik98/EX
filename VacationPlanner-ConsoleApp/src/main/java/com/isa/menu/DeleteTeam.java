1c66e9eb6f469dba629794e64ca5247cef947fa5
PatrykGwardzik <gwardzikpatryk@gmail.com>
2023-01-19 22:38:17 +0100

    DeleteTeam Added

--- VacationPlanner-ConsoleApp/src/main/java/com/isa/menu/DeleteTeam.java
+++ VacationPlanner-ConsoleApp/src/main/java/com/isa/menu/DeleteTeam.java
@@ -1,19 +1,22 @@
 package com.isa.menu;
 
 import com.isa.dataManager.DataManager;
+import com.isa.model.Employee;
 import com.isa.model.Team;
 
 import java.util.Scanner;
 
 public class DeleteTeam {
     public void deleteTeam() {
-        int exit =1;
+        int exit = 1;
         DataManager.loadTeamFromFile();
-        while(exit !=0) {
+        while (exit != 0) {
             System.out.println("Podaj numer zespół który chcesz usunąć:");
             printAllTeams();
-            exit = userInput();
-            DataManager.teamList.remove(exit -1);
+            int selection = userInput();
+            cleanTeamFromEmployee(DataManager.teamList.get(selection).getTeamName());
+            DataManager.teamList.remove(selection);
+            exit = 0;
         }
     }
 
@@ -21,7 +24,15 @@ public class DeleteTeam {
         Scanner scanner = new Scanner(System.in);
         int input;
         input = scanner.nextInt();
-        return input;
+        return input - 1;
+    }
+
+    private void cleanTeamFromEmployee(String teamName) {
+        for (Employee employee : DataManager.employeeList) {
+            if (employee.getTeam().equals(teamName)) {
+                employee.setTeam("UnAssignedTeam");
+            }
+        }
     }
 
     private void printAllTeams() {
@@ -29,7 +40,7 @@ public class DeleteTeam {
         for (Team teamName : DataManager.teamList) {
             System.out.println(i++ + ". " + teamName);
         }
-        System.out.println( "0. " + "Wyjście\n\n");
+        System.out.println("0. " + "Wyjście\n\n");
     }
 }
 
