6076411c2986b4de024ec0166094460b07f7b853
PatrykGwardzik <gwardzikpatryk@gmail.com>
2023-02-11 20:16:26 +0100

    common code added to all html files, working properly

--- VacationPlaner-WebApp/src/main/java/com/isa/vacationplanerwebapp/controller/PageController.java
+++ VacationPlaner-WebApp/src/main/java/com/isa/vacationplanerwebapp/controller/PageController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "/test";
-    }
-
     @GetMapping("/css/styles.css")
     public String getCSS() {
         return "/css/styles.css";
