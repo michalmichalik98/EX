b6264a100e2af74c66bed81429a1c0a034ac6df8
PatrykGwardzik <gwardzikpatryk@gmail.com>
2023-02-23 22:37:43 +0100

    vacation delet shows vacations, dates are mest up

--- VacationPlaner-WebApp/src/main/java/com/isa/vacationplanerwebapp/dataManager/DataManagerVacation.java
+++ VacationPlaner-WebApp/src/main/java/com/isa/vacationplanerwebapp/dataManager/DataManagerVacation.java
@@ -1,22 +1,37 @@
 package com.isa.vacationplanerwebapp.dataManager;
 
 import com.isa.vacationplanerwebapp.model.Vacation;
-import net.fortuna.ical4j.model.Date;
-import net.fortuna.ical4j.model.DateTime;
+
 import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.model.property.Attendee;
-import net.fortuna.ical4j.model.property.DtEnd;
-import net.fortuna.ical4j.model.property.DtStamp;
-import net.fortuna.ical4j.model.property.DtStart;
+import net.fortuna.ical4j.model.property.*;
 import org.springframework.stereotype.Controller;
 
 import java.net.URISyntaxException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 public class DataManagerVacation {
 
+    private final List<VEvent> allVacations;
+
+    public DataManagerVacation() {
+        this.allVacations = new ArrayList<>();
+        createDummyData();
+        createDummyData();
+        createDummyData();
+        createDummyData();
+        createDummyData();
+        createDummyData();
+        createDummyData();
+        createDummyData();
+    }
+
+    public List<VEvent> getAllVacations() {
+        return allVacations;
+    }
 
     public void creatVEvent(Vacation vacation, String id) {
         VEvent vacationEvent = new VEvent();
@@ -31,13 +46,29 @@ public class DataManagerVacation {
             vacationEvent.getProperties().add(new Attendee(id));
             vacationEvent.getProperties().add(dtStart);
             vacationEvent.getProperties().add(dtEnd);
+            allVacations.add(vacationEvent);
 
         } catch (URISyntaxException | ParseException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    public void createDummyData(){
+        VEvent vacationEvent = new VEvent();
+        String startDateString = "20230301T000000Z";
+        String endDateString = "2023035T235959Z";
+        String id = "test";
+        try {
+            DtStart dtStart = new DtStart(startDateString);
+            DtEnd dtEnd = new DtEnd(endDateString);
 
-        System.out.println(vacationEvent.getProperties());
+            vacationEvent.getProperties().add(new Attendee(id));
+            vacationEvent.getProperties().add(dtStart);
+            vacationEvent.getProperties().add(dtEnd);
+            allVacations.add(vacationEvent);
 
+        } catch (URISyntaxException | ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
