ffee9520a324708be5f8f4250bdf3d0455482f3e
Dominik Szlachetka <szlachetka.dominik@gmail.com>
2023-03-22 12:08:53 +0100

    pom.xml modified & added employee logger working

--- VacationPlaner-WebApp/src/main/java/com/isa/vacationplanerwebapp/controller/EmployeeController.java
+++ VacationPlaner-WebApp/src/main/java/com/isa/vacationplanerwebapp/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.isa.vacationplanerwebapp.controller;
 import com.isa.vacationplanerwebapp.dataManager.DataManagerEmployees;
 import com.isa.vacationplanerwebapp.model.Employee;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class EmployeeController {
 
     private final DataManagerEmployees dataManagerEmployees;
+    private static final Logger logger = LogManager.getLogger(EmployeeController.class);
+
     @Autowired
     public EmployeeController(DataManagerEmployees dataManagerEmployees) {
         this.dataManagerEmployees = dataManagerEmployees;
@@ -26,7 +30,7 @@ public class EmployeeController {
 
     @GetMapping("/index")
     public String getIndex(Model model) {
-
+        logger.info("GET /index - main page");
         model.addAttribute("AllEmployees", dataManagerEmployees.getEmployees());
         return "/index";
     }
@@ -34,6 +38,7 @@ public class EmployeeController {
 
     @GetMapping("/employeeAdd")
     public String addEmployee(Model model, @RequestParam(name = "Id", required = false) String Id) {
+        logger.info("GET /employeeAdd - adding new employee");
         Employee employee = new Employee();
         model.addAttribute("employeeKeyAdd", employee);
         return "employeeAdd";
@@ -41,55 +46,58 @@ public class EmployeeController {
     @PostMapping("/employeeAdd")
     public String addEmployeeRequest(@Valid Employee employee, BindingResult result) {
         if (result.hasErrors()) {
+            logger.warn("POST /employeeAdd - failed to add new employee - invalid data");
             return "employeeAdd";
         }
         dataManagerEmployees.addEmployee(employee);
+        logger.info("POST /employeeAdd - new employee added successfully");
         return "redirect:/index";
     }
 
 
     @GetMapping("/employeeDelete")
     public String deleteEmployee(Model model) {
-
+        logger.debug("Entered deleteEmployee method");
         model.addAttribute("AllEmployee", dataManagerEmployees.getEmployees());
         return "/employeeDelete";
     }
     @GetMapping("/employeeDelete/{id}")
     public String deleteEmployeeRequest(@PathVariable(required = false, name="id") String id) {
+        logger.debug("Deleting employee with id {}", id);
         dataManagerEmployees.deleteEmployee(id);
+        logger.debug("Redirecting to /employeeDelete");
         return "redirect:/employeeDelete";
     }
 
 
     @GetMapping("/employeeModify")
     public String modifyEmployee(Model model) {
-
+        logger.info("Entering modifyEmployee method");
         model.addAttribute("AllEmployees", dataManagerEmployees.getEmployees());
-
         model.addAttribute("employeeKeyModify", new Employee());
+        logger.info("Leaving modifyEmployee method");
         return "/employeeModify";
     }
     @GetMapping("/employeeModify/{id}")
     public String displayEmployeeToModify(@PathVariable(required = false, name="id") String id, Model model) {
-
+        logger.info("Entering displayEmployeeToModify method with id = {}", id);
         model.addAttribute("employeeKeyModify", dataManagerEmployees.getEmployeeById(id));
         model.addAttribute("AllEmployees", dataManagerEmployees.getEmployees());
-
+        logger.info("Leaving displayEmployeeToModify method");
         return "employeeModify";
     }
     @PostMapping("/employeeModify")
     public String ModifyEmployeeRequest( Employee employee) {
+        logger.info("Entering ModifyEmployeeRequest method with employee = {}", employee);
         dataManagerEmployees.modifyEmployee(employee);
+        logger.info("Leaving ModifyEmployeeRequest method");
         return "redirect:/employeeModify";
     }
 
     @GetMapping("/employeeList")
     public String ListOfEmployees(Model model) {
-
+        logger.info("Fetching all employees");
         model.addAttribute("AllEmployees", dataManagerEmployees.getEmployees());
         return "/employeeList";
     }
-
-
-
 }
